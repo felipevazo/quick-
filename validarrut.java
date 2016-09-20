a92fa2af80014d204fc6f2b427e8eeefdefb6757
felipevazo <f.vasquez07@ufromail.cl>
2016-09-20 11:15:08 -0300

    Update validarrut.java

--- validarrut.java
+++ validarrut.java
@@ -1,1 +1,7 @@
 
+public boolean validarRut(String rut)
+    {
+        Pattern patron = Pattern.compile("[\\d]{8,8}-[\\d|k]{1,1}");
+        Matcher mat= patron.matcher(rut);
+        return (mat.matches());
+    }
